886f26c83db9890bb796a0a4ba55e22fe1264ca3
Stanley Okorie <okstilo@gmail.com>
2021-05-28 18:55:20 +0100

    removed homepage

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,17 @@
-import { createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers/root-reducer';
-import { composeWithDevTools } from "redux-devtools-extension";
+
 import {persistStore} from 'redux-persist';
 
-// const initialState = {};
-// const composeEnhancers = compose
-// const middlewares = [thunk, logger]
+const initialState = {};
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeWithDevTools;
+const middlewares = [thunk, logger]
 
-// export const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middlewares)));
+export const store = createStore(rootReducer, initialState,composeEnhancers(applyMiddleware(...middlewares)));
 
-// const composeEnhancers = composeWithDevTools(applyMiddleware(...middlewares));
-
-export const store = createStore(rootReducer,
-         compose(applyMiddleware(thunk, logger))
-       );
 export const persistor = persistStore(store);
-
-
-// export default store;
+export default store;
