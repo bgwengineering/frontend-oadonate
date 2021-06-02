551fbcda60cceb01de5a4a23e9fa0332ff79001f
Stanley Okorie <okstilo@gmail.com>
2021-06-02 07:21:43 +0100

    cart change

--- src/store/reducers/Cart.js
+++ src/store/reducers/Cart.js
@@ -10,50 +10,6 @@ const INITIAL_STATE = {
 
 const cart = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-<<<<<<< HEAD
-      case actionTypes.TOGGLE_CART_HIDDEN:
-        return {
-          ...state,
-          hidden: !state.hidden
-        };
-      case actionTypes.CART_HIDDEN:
-        return {
-          ...state,
-          hidden:true
-        }
-      case actionTypes.ADD_ITEM:
-        return {
-          ...state,
-          cartItems: addItemToCart(state.cartItems, action.payload)
-        };
-      case actionTypes.CLEAR_ITEM_FROM_CART:
-        return {
-          ...state,
-          cartItems: state.cartItems.filter(
-            cartItem => cartItem.id !== action.payload.id
-          )
-        };
-      case actionTypes.REMOVE_ITEM:
-        return {
-          ...state,
-          cartItems: removeItemFromCart(state.cartItems, action.payload)
-        };
-      case actionTypes.PLACE_ORDERS_PAYSTACK_SUCCESS:
-        return {
-          ...state,
-          checkoutUrl: action.payload
-        };
-      case actionTypes.PLACE_ORDERS_STRIPE_SUCCESS:
-      case actionTypes.PLACE_ORDERS_PAYSTACK_CLEAR:
-      case actionTypes.CLEAR_CART:
-        return {
-          ...state,
-          cartItems: [],
-          checkoutUrl: ""
-        };
-      default:
-        return state;
-=======
         case actionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
@@ -91,7 +47,6 @@ const cart = (state = INITIAL_STATE, action) => {
             };
         default:
             return state;
->>>>>>> e16c8bd13b0c0007d6a4322759ce835b91488cf5
     }
 }
 
