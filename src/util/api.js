1070773127ef3264fc79ab6ff1657f47d6540575
Hudeh <henryudeh@hudehtech.com>
2021-06-05 21:50:45 +0100

    mapping donations to each funds and disable add to cart if item in cart

--- src/util/api.js
+++ src/util/api.js
@@ -32,6 +32,7 @@ axiosInstance.interceptors.response.use(
                 const tokenParts = JSON.parse(atob(refreshToken.split('.')[1]));
 
                 // exp date in token is expressed in seconds, while now() returns milliseconds:
+                // exp date in token is expressed in seconds, while now() returns milliseconds:
                 const now = Math.ceil(Date.now() / 1000);
                 console.log(tokenParts.exp);
 
